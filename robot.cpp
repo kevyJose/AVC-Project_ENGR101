74eee26da17003246488ee6cbccd072eb45aeb30
kevyJose <65949468+kevyJose@users.noreply.github.com>
2020-06-11 01:02:28 +1200

    Update robot.cpp
    So this code should have the foundation for what need in CORE level of the program. 
    To-Do: 
    need to retstructure into functions / check if currently working properly

--- robot.cpp
+++ robot.cpp
@@ -1,17 +1,50 @@
-
 #include "robot.hpp"
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 40.0;
-    double vRight = 30.0;
-    takePicture();
-    SavePPMFile("i0.ppm",cameraView);
-    while(1){
-      setMotors(vLeft,vRight);   
-      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-       usleep(10000);
+	//set the initial L and R speeds
+    double vLeft = 10.0;
+    double vRight = 10.0;
+    int MidRowValues[cameraView.width];
+    //takePicture();
+    //SavePPMFile("i0.ppm",cameraView);
+    std::cout<<cameraView.width<<" "<<cameraView.height<<"  ";
+    
+    while(1){				
+		takePicture();	
+		
+		double error = 0.0;
+		int num_White = 0;
+		for ( int i = 0; i < cameraView.width; i++ ){
+			
+			int pix = get_pixel(cameraView, 50, i, 3);
+			int isWhite;			
+			
+			if (pix > 250){ isWhite = 1;				
+				error = error + i - 75;
+				num_White++;
+			} else{ isWhite = 0; }
+			//std::cout<<isWhite<<" ";
+			//std::cout<<pix<<" ";
+			
+			MidRowValues[i] = isWhite;	//add the values into array (white or non-white)
+			
+			std::cout<<isWhite<<" ";
+			
+		}
+		
+		if (num_White > 0){
+			vLeft = 20 - 0.3 * (error/num_White);
+			vRight = 20 + 0.3 * (error/num_White);
+		} else { vLeft = -5; vRight = -5; }
+							
+		
+		
+		setMotors(vLeft,vRight);   
+		std::cout<<"error ="<<error<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+		
+		usleep(10000);
   } //while
 
 } // main
