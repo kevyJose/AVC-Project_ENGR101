f11afc82ab6b46abc99d3643de130635a75b6352
struwichri <66051986+struwichri@users.noreply.github.com>
2020-06-11 12:53:58 +1200

    Added to robot.cpp
    Add a method that find the middle of white line and calculates how far off it is from the middle, returns how far off it is. "error" can be used by Daniel for his part on adjusting the motor speed. As the "Error" acts as how intense the motor speed must be adjusted.
    Side note: 
    If the error is positive it must turn right.
    if the error is negative it must turn left.

--- robot.cpp
+++ robot.cpp
@@ -1,17 +1,44 @@
 
 #include "robot.hpp"
+
+double lineMiddle(int values[]){ //calculates the middle of white line
+	int numwhites = 0;
+	int whites = 0;
+	for (int i = 0;i < 150;i++){
+		if (values[i] > 0) {
+			whites = whites+i;
+			numwhites++;
+		}
+	}
+	double whitelinelocation = whites/numwhites; //middle of white line location on line
+	double camMiddle = cameraView.width/2; //the actual middle of the line
+	double errors = whitelinelocation - camMiddle ; //calculates the how far of the line is from the middle
+	return errors; 
+};
+
+int middle[150];
+
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 40.0;
-    double vRight = 30.0;
-    takePicture();
+    double vLeft = 20.0;
+    double vRight = 20.0;
     SavePPMFile("i0.ppm",cameraView);
     while(1){
+	  takePicture();	
+	  for (int i =0; i<150;i++){
+		  int pix = get_pixel(cameraView,50,i,3);
+		  int isWhite;
+		  if (pix >250){isWhite = 1;} else {isWhite = 0;}
+		  middle[i] = isWhite;
+		 // std::cout<<middle[i]<<" ";
+		  }
+	  double error = lineMiddle(middle);
+	//  std::cout<<"Error: "<<error<<std::endl;
       setMotors(vLeft,vRight);   
-      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-       usleep(10000);
   } //while
 
-} // main
+} // main 
+
+
