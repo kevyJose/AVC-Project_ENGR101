261af814301c497e7dbda9c0f5056655affd011a
LeeScanlon <66281380+LeeScanlon@users.noreply.github.com>
2020-06-12 12:00:20 +1200

    Update robot.cpp
    This code makes the robot turn around when it hits a dead end in the comp maze. It's not perfect and can be improved but for now it works with the comp maze.

--- robot.cpp
+++ robot.cpp
@@ -1,52 +1,52 @@
 #include "robot.hpp"
+
+double lineMiddle(int values[]){ //calculates the middle of white line
+	int numwhites = 0;
+	int whites = 0;
+	for (int i = 0;i < 150;i++){
+		if (values[i] > 0) {
+			whites = whites+i;
+			numwhites++;
+		}
+	}
+	if(numwhites == 0){ //If there are no white pixels detected then turn
+		return (20);
+	}
+	double whitelinelocation = whites/numwhites; //middle of white line location on line
+	double camMiddle = cameraView.width/2; //the actual middle of the line
+	double errors = whitelinelocation - camMiddle ; //calculates the how far of the line is from the middle
+	return errors; 
+};
+
+int middle[150];
+
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-	//set the initial L and R speeds
-    double vLeft = 10.0;
-    double vRight = 10.0;
-    int MidRowValues[cameraView.width];
-    //takePicture();
-    //SavePPMFile("i0.ppm",cameraView);
-    std::cout<<cameraView.width<<" "<<cameraView.height<<"  ";
-    
-    while(1){				
-		takePicture();	
-		
-		double error = 0.0;
-		int num_White = 0;
-		for ( int i = 0; i < cameraView.width; i++ ){
-			
-			int pix = get_pixel(cameraView, 50, i, 3);
-			int isWhite;			
-			
-			if (pix > 250){ isWhite = 1;				
-				error = error + i - 75;	//calculate error value based on position of WHITE line
-				num_White++;
-			} else{ isWhite = 0; }
-			//std::cout<<isWhite<<" ";
-			//std::cout<<pix<<" ";
-			
-			MidRowValues[i] = isWhite;	//add the values into array (white or non-white)
-			
-			std::cout<<isWhite<<" ";
-			
-		}
-	    	//Adjust position to maintain line in middle of frame
-		//adjust left and right speeds, based on error
-		if (num_White > 0){			
-			vLeft = 20 - 0.3 * (error/num_White);
-			vRight = 20 + 0.3 * (error/num_White);
-		//no white line detected...reverse
-		} else { vLeft = -5; vRight = -5; }
-							
-		
-		
-		setMotors(vLeft,vRight);   
-		std::cout<<"error ="<<error<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-		
-		usleep(10000);
+    double vLeft = 20.0;
+    double vRight = 20.0;
+    SavePPMFile("i0.ppm",cameraView);
+    while(1){
+	  takePicture();	
+	  for (int i =0; i<150;i++){
+		  int pix = get_pixel(cameraView,50,i,3);
+		  int isWhite;
+		  if (pix >250){isWhite = 1;} else {isWhite = 0;}
+		  middle[i] = isWhite;
+		 // std::cout<<middle[i]<<" ";
+		  }
+	  double error = lineMiddle(middle);
+	    //this is the code that made it work
+		if (error > 0){
+			vLeft = 20+0.3*error;
+			vRight = 20-0.3*error;
+		} else {
+			vLeft = 20;
+			vRight = 20;
+			}
+	//  std::cout<<"Error: "<<error<<std::endl;
+      setMotors(vLeft,vRight);   
   } //while
 
-} // main
+} // main 
